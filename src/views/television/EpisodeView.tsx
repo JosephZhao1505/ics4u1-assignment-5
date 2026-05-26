c813192c7cce9071b02e5bd091a35795c8fb82f1
Joseph <joseph.zhao150@outlook.com>
2026-05-26 09:57:08 -0400

    Pricing added to seasons

--- src/views/television/EpisodeView.tsx
+++ src/views/television/EpisodeView.tsx
@@ -8,6 +8,7 @@ import { useTmdb } from "@/hooks";
 export const EpisodeView = () => {
   const { id, seasonNumber } = useParams();
   const { data } = useTmdb<SeasonsResponse>(`${TELEVISION_ENDPOINT}/${id}/season/${seasonNumber}`, {});
+  let priceDisplay = null;
 
   const gridData = (data?.episodes ?? []).map((result) => ({
     id: result.id,
@@ -20,10 +21,32 @@ export const EpisodeView = () => {
     return <p className="text-center text-gray-400">Loading...</p>;
   }
 
+  if (data.air_date) {
+    const currentYear = new Date().getFullYear();
+    const releaseDate = new Date(data.air_date).getFullYear();
+    const mediaAge = currentYear - releaseDate;
+
+    const basePrice = 19.99;
+    const floorPrice = 4.99;
+
+    const finalPrice = basePrice - mediaAge;
+
+    priceDisplay = Math.max(finalPrice, floorPrice);
+  }
+
   return (
     <section className="px-2">
       <div className="flex-1 space-y-4">
-        <h1 className="font-bold text-3xl">{data?.name}</h1>
+        <div className="mb-4 flex items-start justify-between gap-4">
+          <div>
+            <h1 className="font-bold text-3xl">{data.name}</h1>
+          </div>
+
+          <div className="flex shrink-0 flex-col items-end">
+            {!priceDisplay ? "" : <p className="mb-2 font-bold text-3xl">${priceDisplay.toFixed(2)}</p>}
+            {/* button here */}
+          </div>
+        </div>
         <p className="flex items-center gap-2 text-gray-400">
           <FaCalendarAlt />
           {data?.air_date}
